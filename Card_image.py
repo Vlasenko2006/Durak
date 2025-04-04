a37ad83b3b3159f1ecd19c201e9955bc89aa04a3
Andrey <andrey.vlasenko2006@gmail.com>
2025-04-04 07:30:45 +0200

    split the logics

--- Card_image.py
+++ Card_image.py
@@ -19,9 +19,12 @@ suit_colors = {
 class Card_image:
     def __init__(self, 
                  card_width=150,
-                 card_height=200):
+                 card_height=200,
+                 distance =2.5):
         self.card_width = card_width
         self.card_height = card_height
+        self.row_spacing = int(distance * self.card_height)
+        self.frame_height = self.card_height * 2 + self.row_spacing + 20
 
     def create_card_image(self, rank, suit):
         width, height = self.card_width, self.card_height
@@ -81,33 +84,70 @@ class Card_image:
 
         return card_image
 
-    def create_closed_cards(self, card_plotter, num_closed_cards):
-        for i in range(num_closed_cards):
+    def create_closed_cards(self, opponent_cards,image_refs, upper_card_labels,frame):
+        upper_card_labels.clear()
+        for i in range(len(opponent_cards)):
             closed_card_image = self.create_closed_card_image()
             closed_card_photo = ImageTk.PhotoImage(closed_card_image)
-            label = tk.Label(card_plotter.frame, image=closed_card_photo, bg='grey')
+            label = tk.Label(frame, image=closed_card_photo, bg='grey')
             label.image = closed_card_photo  # Store reference in label
             label.grid(row=0, column=i, padx=10, pady=10, sticky="n")
-            card_plotter.image_refs.append(closed_card_photo)  # Store reference to prevent garbage collection
-            card_plotter.upper_card_labels.append(label)  # Store reference to upper row card labels
+            image_refs.append(closed_card_photo)  # Store reference to prevent garbage collection
+            upper_card_labels.append(label)  # Store reference to upper row card labels
 
         # Center the closed cards
-        card_plotter.frame.grid_columnconfigure((0, num_closed_cards - 1), weight=1)
+        frame.grid_columnconfigure((0, len(opponent_cards) - 1), weight=1)
 
-    def create_open_cards(self, card_plotter, my_cards, num_open_cards, row_spacing, on_card_click):
-        for i in range(num_open_cards):
-            rank = my_cards[i % len(my_cards)][0]
-            suit = my_cards[i % len(my_cards)][1]
+    # def create_open_cards(self, card_plotter, my_cards, num_open_cards, row_spacing, on_card_click):
+    #     for i in range(num_open_cards):
+    #         rank = my_cards[i % len(my_cards)][0]
+    #         suit = my_cards[i % len(my_cards)][1]
+    #         card_image = self.create_card_image(rank, suit)
+    #         card_photo = ImageTk.PhotoImage(card_image)
+    #         label = tk.Label(card_plotter.frame, image=card_photo, bg='grey')
+    #         label.image = card_photo  # Store reference in label
+    #         label.grid(row=2, column=i, padx=10, pady=(row_spacing, 10), sticky="n")
+    #         label.bind("<Button-1>", on_card_click)  # Bind left mouse button click event
+    #         card_plotter.image_refs.append(card_photo)  # Store reference to prevent garbage collection
+    #         card_plotter.lower_card_labels.append(label)  # Store reference to lower row card labels
+    #         # Store the card information in the label
+    #         label.card_info = (rank, suit)
+
+        # # Center the open cards
+        # card_plotter.frame.grid_columnconfigure((0, num_open_cards - 1), weight=1)
+        
+
+
+
+    def create_open_cards(self,my_cards, image_refs,lower_card_labels, on_card_click, frame):
+        lower_card_labels.clear()
+        for i in range(len(my_cards)):
+            rank = my_cards[i][0]
+            suit = my_cards[i][1]
             card_image = self.create_card_image(rank, suit)
             card_photo = ImageTk.PhotoImage(card_image)
-            label = tk.Label(card_plotter.frame, image=card_photo, bg='grey')
+            label = tk.Label(frame, image=card_photo, bg='grey')
             label.image = card_photo  # Store reference in label
-            label.grid(row=2, column=i, padx=10, pady=(row_spacing, 10), sticky="n")
+            label.grid(row=2, column=i, padx=10, pady=(self.row_spacing, 10), sticky="n")
             label.bind("<Button-1>", on_card_click)  # Bind left mouse button click event
-            card_plotter.image_refs.append(card_photo)  # Store reference to prevent garbage collection
-            card_plotter.lower_card_labels.append(label)  # Store reference to lower row card labels
-            # Store the card information in the label
+            image_refs.append(card_photo)  # Store reference to prevent garbage collection
+            lower_card_labels.append(label)  # Store reference to lower row card labels
+           # Store the card information in the label
             label.card_info = (rank, suit)
 
-        # Center the open cards
-        card_plotter.frame.grid_columnconfigure((0, num_open_cards - 1), weight=1)
+
+        
+    def draw_trump_card_perpendicular(self, 
+                                          trump,
+                                          no_more_cards_left,   
+                                          image_refs,
+                                          table,
+                                          frame):
+        if not no_more_cards_left:
+            trump_card_image = self.create_card_image(trump[0] ,trump[1])
+            trump_card_image = trump_card_image.rotate(90, expand=True)
+            trump_card_photo = ImageTk.PhotoImage(trump_card_image)
+            label = tk.Label(frame, image=trump_card_photo, bg='grey')
+            label.image = trump_card_photo
+            label.place(in_= table, relx=-0.5, rely=0.5, anchor=tk.CENTER)
+            image_refs.append(trump_card_photo)
